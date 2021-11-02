7c7c3a2dd00b9c67ad8a8802a35787bfea3d386f
salickaite <90153125+salickaite@users.noreply.github.com>
2021-11-02 20:04:59 +0200

    Delete compare_alphabet.cpp

--- compare_alphabet.cpp
+++
@@ -1,7 +0,0 @@
-#include "compare_alphabet.h"
-
-bool compare_alphabet(studentas a, studentas b)
-{
-    if (a.pavarde != b.pavarde) return a.pavarde < b.pavarde;
-    else return a.vardas < b.vardas;
-}
