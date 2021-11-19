5baa0a9e1410b2e070301b1de22d8f288207f201
salickaite <90153125+salickaite@users.noreply.github.com>
2021-11-19 20:13:00 +0200

    Add files via upload

---
+++ write.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "studentas.h"
+#include <vector>
+#include <list>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+void v_write(std::vector<studentas>& v, std::string pav);
+
+void l_write(std::list<studentas>& l, std::string pav);
